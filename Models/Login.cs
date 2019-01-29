ae855a9b4beb12a12c0234ac982beb9164531719
Vivek Kumar <vivekkumarnxt@gmail.com>
2019-01-29 14:22:40 +0545

    hello

--- Models/Login.cs
+++ Models/Login.cs
@@ -15,6 +15,7 @@ namespace LoginApplication.Models
         public string Koju { get; set; }
         public string email { get; set; }
         public string Address { get; set; }
+        public string Passang { get; set; }
 
     }
 }
