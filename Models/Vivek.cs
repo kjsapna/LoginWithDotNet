27085cbc4fca49af85d72e8621afc082cc382d2b
Vivek Kumar <vivekkumarnxt@gmail.com>
2019-01-29 12:23:40 +0545

    New Commit

---
+++ Models/Vivek.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApplication.Models
+{
+    public class Vivek
+    {
+        public int Id {get; set; }
+        public string Name { get; set; }
+    }
+}
