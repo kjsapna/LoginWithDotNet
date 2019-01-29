cc57aae296d0f7fff96a24da96834293d73d8f57
kjsapna <sapana.koju@gmail.com>
2019-01-29 12:04:03 +0545

    model added

---
+++ Models/SystemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApplication.Models
+{
+    public class SystemModel
+
+    {
+        public int Number { get; set; }
+        public string FirstName { get; set; }
+
+    }
+}
