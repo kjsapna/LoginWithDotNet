a1cbea495a636e9e13e7af6e026c69844e27669b
Vivek Kumar <vivekkumarnxt@gmail.com>
2019-01-29 12:30:20 +0545

    New field created in Vivek class

--- Models/Vivek.cs
+++ Models/Vivek.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 namespace LoginApplication.Models
 {
-    public class Vivek
-    {
-        public int Id {get; set; }
-        public string Name { get; set; }
+	public class Vivek
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string Phone No {get ;set;}
     }
 }
